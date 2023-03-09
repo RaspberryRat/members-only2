6ac3715db0385a83cec9dac0fb541fe6c380e00e
G <mail@grahamirvine.com>
2023-03-09 16:09:22 -0700

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :username, presence: true, uniqueness: true, length: { in 3..12 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
 end
