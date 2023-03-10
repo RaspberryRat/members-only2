b3b58af0ee89dbf42b63e6f57c001fc68ae799b2
G <mail@grahamirvine.com>
2023-03-09 18:22:12 -0700

    links to sign up and sign in working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,5 +28,4 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :body)
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "posts#index"
+  root to: "posts#index"
 
+  get "posts/new", to: "posts#new"
   get "posts/:id", to: "posts#show"
 
   resources :posts, only: [:new, :create, :index, :show]
+  # resources :users, only: [:destroy]
 
 
 end
