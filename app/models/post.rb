eef100741ec702492f4367da57be668b37247d92
G <mail@grahamirvine.com>
2023-03-10 17:35:38 -0700

    improved styling on index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
 
-  validates :title, presence: true, uniqueness: true, length: { in: 3..30 }
+  validates :title, presence: true, uniqueness: true, length: { in: 3..50 }
   validates :body, presence: true, length: { in: 10..10000 }
 
   belongs_to :user
