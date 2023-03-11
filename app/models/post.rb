51508b136a511f8f5e18b5c48949d91ea75c9b7f
G <mail@grahamirvine.com>
2023-03-10 17:13:16 -0700

    changed max body length

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true, length: { in: 3..30 }
-  validates :body, presence: true, length: { in: 10..1000 }
+  validates :body, presence: true, length: { in: 10..10000 }
 
   belongs_to :user
 end
